9ccc0659b1d25a5c11c386913d76cc74c4a59649
davewoman195 <67042217+davewoman195@users.noreply.github.com>
2021-07-20 02:45:19 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<ThemeProvider
 			attribute='class'
-			defaultTheme='dark'
+			defaultTheme='black'
 			disableTransitionOnChange
 		>
 			<Meta />
